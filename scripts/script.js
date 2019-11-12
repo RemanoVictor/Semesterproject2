f96c1b3496d9d778f2dca61b56720327ec8651aa
Remano <remanovictor@icloud.coms>
2019-11-12 16:35:04 +0100

    also started Adobe XD file

--- scripts/script.js
+++ scripts/script.js
@@ -13,25 +13,49 @@ fetch ('https://anapioficeandfire.com/api/characters/')
 });
 }
 
+// modal
+
+
+// modal pop up
+
+
+
+
+//const btn = document.getElementById("showModal");
+
+
+
+
+
+
+
 //dice roll
 
 function rollDice(){
 
+const modal = document.getElementById("modalPopUp");
+let close = document.getElementsByClassName("close")[0];
+
+let rollOutput = document.getElementById("rollOutput");
 let max = 6;
 
 let roll = Math.ceil(Math.random()*max);
 console.log("you rolled a " + roll+"!");
-
-let rollOutput = document.getElementById("rollOutput");
-
 rollOutput.innerHTML = " You rolled a " + roll+ " !"; 
 
 if(roll == 6 ){
-    alert("Well Done, have another go!");
+    
+        modal.style.display = "block";
+      
+    //document.getElementById("alert").innerHTML="The Gods have smiled upon your fortune! Have another go";
 }
 
 else{
-    document.getElementById("alert").innerHTML="";
+    //document.getElementById("alert").innerHTML="";
+    close.onclick = function() {
+        modal.style.display = "none";
+      };
+    modal.style.display = "none";
 }
 
 }
