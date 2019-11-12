e3d368b65b1f5ec6b07f8422693734bf00755d21
Remano <remanovictor@icloud.coms>
2019-11-12 14:36:35 +0100

    added dice roll javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 
-
+function callApi(){
 fetch ('https://anapioficeandfire.com/api/characters/')
 
 .then ((response)=>{
@@ -11,3 +11,27 @@ fetch ('https://anapioficeandfire.com/api/characters/')
     console.log(data);
     
 });
+}
+
+//dice roll
+
+function rollDice(){
+
+let max = 6;
+
+let roll = Math.ceil(Math.random()*max);
+console.log("you rolled a " + roll+"!");
+
+let rollOutput = document.getElementById("rollOutput");
+
+rollOutput.innerHTML = " You rolled a " + roll+ " !"; 
+
+if(roll == 6 ){
+    alert("Well Done, have another go!");
+}
+
+else{
+    document.getElementById("alert").innerHTML="";
+}
+
+}
