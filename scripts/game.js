d081b4e63fc31e14726f9cace4c51a83f6ce11b4
Remano <remanovictor@icloud.coms>
2019-11-18 12:55:20 +0100

    fixed the player switch code :)

--- scripts/game.js
+++ scripts/game.js
@@ -1,44 +1,55 @@
 //Board Game
-
 playerTurn = {};
 
 function rollDice() {
-    let rollOutput = document.getElementById("rollOutput");
+
     //let roll = Math.ceil(Math.random()*max);
     let diceRoll = (Math.floor(Math.random() * 6) + 1);
     console.log("you rolled a " + diceRoll + "!");
+    let rollOutput = document.getElementById("rollOutput");
 
     rollOutput.innerHTML = " You rolled a " + diceRoll + " !";
 
     // board mechanics
-    (playerOne.turn === true) ? playerTurn = playerOne: playerTurn = playerTwo;
-    
-    if(playerOne.position != playerOne.position){
-        document.getElementById('tile'+playerTurn.position).innerHTML="";
+    if (playerOne.turn === true) {
+        playerTurn = playerOne;
+    } else {
+        playerTurn = playerTwo
     }
+
+    if (playerTurn.tileNum != playerTurn.tileNum) {
+        document.getElementById('tile' + playerTurn.tileNum);
+    }
+
+    console.log(playerTurn);
+
+
     //dice mechanics
     if (diceRoll === 6) {
         alert('well done');
-        playerTurn.position = playerTurn.position + diceRoll;
+        playerTurn.tileNum = playerTurn.tileNum + diceRoll;
         rollDice()
-    } 
-    else {
-        playerTurn.position = playerTurn.position + diceRoll;
-        
-        console.log(playerTurn.position);
-        document.getElementById('tile'+playerTurn.position).innerHTML=`
+    } else {
+        playerTurn.tileNum = playerTurn.tileNum + diceRoll;
+
+        console.log(playerTurn.tileNum);
+        document.getElementById('tile' + playerTurn.tileNum).innerHTML = `
             <img src="${playerTurn.token}">
         `;
     }
+
     //Switch players
-    if (playerTurn.turn === true) {
+    if (playerTurn === playerOne) {
         playerOne.turn = false;
         playerTwo.turn = true;
     } else {
-        playerOne.turn = true;
         playerTwo.turn = false;
+        playerOne.turn = true;
     }
-    console.log(playerTurn);
-    
+
+
+
+
+
 
 }
