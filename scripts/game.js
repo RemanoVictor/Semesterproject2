812fb951a1c1072e4bdb5ccda96757771449437e
Remano <remanovictor@icloud.coms>
2019-11-19 11:10:13 +0100

    fixed token moving

--- scripts/game.js
+++ scripts/game.js
@@ -1,14 +1,14 @@
 //Board Game
 playerTurn = {};
 
+function placeTokens(){
+    document.getElementById('tile0').innerHTML+=`<img src="${playerOne.token}"/>`
+    document.getElementById('tile0').innerHTML+=`<img src="${playerTwo.token}"/>`
+}
+
+placeTokens();
+
 function rollDice() {
-
-    //let roll = Math.ceil(Math.random()*max);
-    let diceRoll = (Math.floor(Math.random() * 6) + 1);
-    console.log("you rolled a " + diceRoll + "!");
-    let rollOutput = document.getElementById("rollOutput");
-
-    rollOutput.innerHTML = " You rolled a " + diceRoll + " !";
 
     // board mechanics
     if (playerOne.turn === true) {
@@ -17,11 +17,31 @@ function rollDice() {
         playerTurn = playerTwo
     }
 
-    if (playerTurn.tileNum != playerTurn.tileNum) {
-        document.getElementById('tile' + playerTurn.tileNum);
+    if(playerOne.tileNum !== playerTwo.tileNum){
+        document.getElementById('tile'+ playerTurn.tileNum).innerHTML="";
+    }
+    else{
+        if(playerOne.turn === true){
+            document.getElementById('tile'+playerTurn.tileNum).innerHTML += "";            
+            document.getElementById('tile'+playerTurn.tileNum).innerHTML += `<img src="${playerTwo.token}"/>`;
+        }
+        else{
+            document.getElementById('tile'+playerTurn.tileNum).innerHTML += "";            
+            document.getElementById('tile'+playerTurn.tileNum).innerHTML += `<img src="${playerOne.token}"/>`;
+        }        
     }
 
-    console.log(playerTurn);
+    console.log(playerTurn.tileNum);
+    console.log(document.getElementById('tile'+playerTurn.tileNum).innerHTML="");
+
+
+    //let roll = Math.ceil(Math.random()*max);
+    let diceRoll = (Math.floor(Math.random() * 6) + 1);
+    console.log("you rolled a " + diceRoll + "!");
+    let rollOutput = document.getElementById("rollOutput");
+
+    rollOutput.innerHTML = " You rolled a " + diceRoll + " !";
+
 
 
     //dice mechanics
@@ -31,52 +51,44 @@ function rollDice() {
         rollDice()
     } else {
         playerTurn.tileNum = playerTurn.tileNum + diceRoll;
-
-        //console.log(playerTurn.tileNum);
+        console.log(playerTurn.tileNum);
         document.getElementById('tile' + playerTurn.tileNum).innerHTML = `
             <img src="${playerTurn.token}">
         `;
     }
 
+
     //trap mechanics
 
-    if(playerTurn.tileNum === trapOne.tileNum){
+    if (playerTurn.tileNum === trapOne.tileNum) {
         alert(trapOne.message);
         playerTurn.tileNum = playerTurn.tileNum - trapOne.penalty;
         document.getElementById("tile" + playerTurn.tileNum).innerHTML = `
             <img src="${playerTurn.token}">
         `;
         console.log(playerTurn.tileNum);
-    }
-
-    if(playerTurn.tileNum === trapTwo.tileNum){
+    } else if (playerTurn.tileNum === trapTwo.tileNum) {
         alert(trapTwo.message);
         playerTurn.tileNum = playerTurn.tileNum - trapTwo.penalty;
         document.getElementById("tile" + playerTurn.tileNum).innerHTML = `
             <img src="${playerTurn.token}">
         `;
         console.log(playerTurn.tileNum);
-    }
-
-    if(playerTurn.tileNum === trapThree.tileNum){
+    } else if (playerTurn.tileNum === trapThree.tileNum) {
         alert(trapThree.message);
         playerTurn.tileNum = playerTurn.tileNum + trapThree.penalty;
         document.getElementById("tile" + playerTurn.tileNum).innerHTML = `
             <img src="${playerTurn.token}">
         `;
         console.log(playerTurn.tileNum);
-    }
-
-    if(playerTurn.tileNum === trapFour.tileNum){
+    } else if (playerTurn.tileNum === trapFour.tileNum) {
         alert(trapThree.message);
         playerTurn.tileNum = playerTurn.tileNum - trapFour.penalty;
         document.getElementById("tile" + playerTurn.tileNum).innerHTML = `
             <img src="${playerTurn.token}">
         `;
         console.log(playerTurn.tileNum);
-    }
-
-    if(playerTurn.tileNum === trapFive.tileNum){
+    } else if (playerTurn.tileNum === trapFive.tileNum) {
         alert(trapThree.message);
         playerTurn.tileNum = playerTurn.tileNum - trapFive.penalty;
         document.getElementById("tile" + playerTurn.tileNum).innerHTML = `
