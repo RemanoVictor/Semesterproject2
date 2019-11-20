a6c70311edb89ef618ea713e5c40643bb8f10f0a
Remano <remanovictor@icloud.coms>
2019-11-20 20:17:55 +0100

    changed name of player.js to store .js
    changede name of player.js to store .js as it contains more than just the players.
    
    added seperate script containing trap mechanics named trap.js.

--- scripts/game.js
+++ scripts/game.js
@@ -1,24 +1,19 @@
 //Board Game
-let playerTurn = {};
-
 function placeTokens() {
     document.getElementById('tile0').innerHTML += `<img src="${playerOne.token}"/>`
     document.getElementById('tile0').innerHTML += `<img src="${playerTwo.token}"/>`
 }
-
 placeTokens();
 
 function rollDice() {
-
+    let playerTurn = {};
     // board mechanics
     if (playerOne.turn === true) {
         playerTurn = playerOne;
     } else {
         playerTurn = playerTwo
     }
-
     // if players share a tile
-
     if (playerOne.tileNum !== playerTwo.tileNum) {
         document.getElementById('tile' + playerTurn.tileNum).innerHTML = "";
     } else {
@@ -47,43 +42,6 @@ function rollDice() {
             <img src="${playerTurn.token}">
         `;
     }
-    //trap mechanics
-    if (playerTurn.tileNum === trapOne.tileNum) {
-        alert(trapOne.message);
-        playerTurn.tileNum = playerTurn.tileNum - trapOne.penalty;
-        document.getElementById("tile" + playerTurn.tileNum).innerHTML = `
-            <img src="${playerTurn.token}">
-        `;
-        console.log(playerTurn.tileNum);
-    } else if (playerTurn.tileNum === trapTwo.tileNum) {
-        alert(trapTwo.message);
-        playerTurn.tileNum = playerTurn.tileNum - trapTwo.penalty;
-        document.getElementById("tile" + playerTurn.tileNum).innerHTML = `
-            <img src="${playerTurn.token}">
-        `;
-        console.log(playerTurn.tileNum);
-    } else if (playerTurn.tileNum === trapThree.tileNum) {
-        alert(trapThree.message);
-        playerTurn.tileNum = playerTurn.tileNum + trapThree.penalty;
-        document.getElementById("tile" + playerTurn.tileNum).innerHTML = `
-            <img src="${playerTurn.token}">
-        `;
-        console.log(playerTurn.tileNum);
-    } else if (playerTurn.tileNum === trapFour.tileNum) {
-        alert(trapThree.message);
-        playerTurn.tileNum = playerTurn.tileNum - trapFour.penalty;
-        document.getElementById("tile" + playerTurn.tileNum).innerHTML = `
-            <img src="${playerTurn.token}">
-        `;
-        console.log(playerTurn.tileNum);
-    } else if (playerTurn.tileNum === trapFive.tileNum) {
-        alert(trapThree.message);
-        playerTurn.tileNum = playerTurn.tileNum - trapFive.penalty;
-        document.getElementById("tile" + playerTurn.tileNum).innerHTML = `
-            <img src="${playerTurn.token}">
-        `;
-        console.log(playerTurn.tileNum);
-    }
     //Switch players
     if (playerTurn === playerOne) {
         playerOne.turn = false;
@@ -92,9 +50,4 @@ function rollDice() {
         playerTwo.turn = false;
         playerOne.turn = true;
     }
-
-
-
-
-
 }
--- scripts/store.js
+++ scripts/store.js
@@ -1,44 +1,39 @@
-let playerOne={
+const playerOne={
     tileNum:0,
     turn:true,
     token:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3f2rl4ga6_NOJ5i7fZ-sebBe64iDlxuEFCR071x0fYLgSbGeX&s',
     test:'play1'    
 };
 
-let playerTwo={    
+const playerTwo={    
     tileNum:0,
     turn:false,
     token:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYj7gZpmsyyv-KSPy1zWVvXGvyjAlu-QFqaNUE1jwBRbNXThG3&s',
     test:'play2'    
 };
-            
-
-let trapOne={
+           
+//trap declaration
+const trapOne={
     tileNum:3,
     penalty:3,
     message:" You stepped on a lego, move back 3 spaces "
 }
-
-let trapTwo={
+const trapTwo={
     tileNum:12,
     penalty:4,
     message:" Seriously dude, wtf "
 }
-
-
-let trapThree={
+const trapThree={
     tileNum:17,
     penalty:5,
     message:" A midget carries you a few steps, Move forward 6 steps "
 }
-
-let trapFour={
+const trapFour={
     tileNum:24,
     penalty:2,
     message:" Dragon steps on yout toe, move back 2 spaces "
 }
-
-let trapFive={
+const trapFive={
     tileNum:29,
     penalty:10,
     message:" aaaah, so close! buht bye bye "
