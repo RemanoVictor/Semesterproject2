ffa0e0b996777ba87af6718605ec8d90a899c3fd
Remano <remanovictor@icloud.coms>
2019-12-09 20:48:15 +0100

    all styling done, javascript is in working order, just a few touch ups here and h

--- scripts/winner.js
+++ scripts/winner.js
@@ -2,13 +2,11 @@ const winnerToken = document.getElementById('winnerToken');
 const winnerDetails = document.getElementById('winnerDetails');
 const winnerDescription = document.getElementById('winnerDescription');
 
+winnerDetails.innerHTML=`
+<h2> ...Congratulations ${sessionStorage.getItem('winnerName')}...</h2>
+`;
 winnerToken.innerHTML+=`
 <img src="media/tokens/${sessionStorage.getItem("winnerName")}.svg">`;
-
 winnerDescription.innerHTML = `
-<p> And so, as the dust settled on the battlefield, one person stood victorious amongst the carnage. ${sessionStorage.getItem('winnerName')} has, against all odds, claimed The Iron Throne.
-`;
-
-winnerDetails.innerHTML=`
-<h3> Congratulations ${sessionStorage.getItem('winnerName')}</h3>
+<p> And so, as the dust settled on the battlefield, one warrior stood victorious amongst the carnage. ${sessionStorage.getItem('winnerName')}, against all odds, has claimed The Iron Throne.
 `;
