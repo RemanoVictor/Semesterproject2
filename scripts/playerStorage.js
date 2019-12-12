916e485b683b6f8ee56cf65149db26e887f0b1e6
Remano <remanovictor@icloud.coms>
2019-12-12 20:15:57 +0100

    dummy proofing

--- scripts/playerStorage.js
+++ scripts/playerStorage.js
@@ -3,7 +3,7 @@ let count = 0;
 function saveChar(name) {
     closeModal();
     if (count === 0) {
-        document.getElementById('iconSelector').innerHTML="Player 2: click an icon to select a character ";
+        document.getElementById('iconSelector').innerHTML="PLAYER 2: Click an icon to select a character ";
         sessionStorage.setItem("playerOne", name);
         sessionStorage.setItem("icon", '/media/tokens/' + name + '.svg');
         document.getElementById(name).setAttribute("class", "[ animated flipOutY ]");
