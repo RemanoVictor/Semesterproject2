e7346547a4889556ecd65a749abeb64166880945
Remano <remanovictor@icloud.coms>
2020-04-23 09:19:08 +0200

    fixing character link

--- scripts/playerStorage.js
+++ scripts/playerStorage.js
@@ -1,18 +1,21 @@
 let count = 0;
 
 function saveChar(name) {
-    closeModal();
-    if (count === 0) {
-        document.getElementById('iconSelector').innerHTML="PLAYER 2: Click an icon to select a character ";
-        sessionStorage.setItem("playerOne", name);
-        sessionStorage.setItem("icon", '/media/tokens/' + name + '.svg');
-        document.getElementById(name).setAttribute("class", "[ animated flipOutY ]");
-        count++;
-    } else {        
-        sessionStorage.setItem("playerTwo", name)
-        sessionStorage.setItem("icon2", "/media/tokens/" + name + '.svg');
-        if (count === 1) {
-            window.location = "http://127.0.0.1:5500/boardgame.html";
-        }
+  closeModal();
+  if (count === 0) {
+    document.getElementById("iconSelector").innerHTML =
+      "PLAYER 2: Click an icon to select a character ";
+    sessionStorage.setItem("playerOne", name);
+    sessionStorage.setItem("icon", "/media/tokens/" + name + ".svg");
+    document
+      .getElementById(name)
+      .setAttribute("class", "[ animated flipOutY ]");
+    count++;
+  } else {
+    sessionStorage.setItem("playerTwo", name);
+    sessionStorage.setItem("icon2", "/media/tokens/" + name + ".svg");
+    if (count === 1) {
+      window.location = "/boardgame.html";
     }
+  }
 }
